1ad35ca428ce16e29baa1d3f62fe09217675c46b
JoaoLucasdCB <joaolucascosta32@gmail.com>
2025-05-27 20:00:35 -0300

    consertando erros model motos

--- RideMoto/src/main/java/InoDev/RideMoto/Models/MotosModel.java
+++ RideMoto/src/main/java/InoDev/RideMoto/Models/MotosModel.java
@@ -10,14 +10,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
 
